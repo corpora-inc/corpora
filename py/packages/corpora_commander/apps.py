375441a9f97b4ffad7c9d2c0fcb69efd316a2cde
Skylar Saveland <skylar.saveland@gmail.com>
2025-06-27 00:30:50 +0000

    Corpora Commander

--- py/packages/corpora_commander/apps.py
+++ py/packages/corpora_commander/apps.py
@@ -3,4 +3,6 @@ from django.apps import AppConfig
 
 class CorporaCommanderConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
+    # name = "Corpora Commander"
     name = "corpora_commander"
+    verbose_name = "Corpora Commander"
